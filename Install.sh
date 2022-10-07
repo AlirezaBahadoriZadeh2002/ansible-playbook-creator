42ee360a507d63b59f6533595a756222049d7ebd
AlirezaBahadoriZadeh <101427194+AlirezaBahadoriZadeh2002@users.noreply.github.com>
2022-10-07 19:06:25 +0330

    Delete Install.sh

--- Install.sh
+++
@@ -1,8 +0,0 @@
-yum install bash-completion -y
-bash
-hostnamectl set-hostname Ansible
-yum install epel-release -y
-yum install -y ansible
-ansible --version
-#adhoc command:
-ansible -m ping localhost
